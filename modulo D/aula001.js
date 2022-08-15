c5361ae77420faacc613f2d5771b298d3c8d24ec
3vinicius <amorimvinicius3@gmail.com>
2022-08-15 10:52:42 -0300

    Exercicios de js

---
+++ modulo D/aula001.js
@@ -0,0 +1,6 @@
+var vel = 30.0
+console.log(`A velocidade do veiculo é de ${vel}Km`)
+if (vel >= 50){
+    console.log('Você ultapassou a maxima. Multado !!!')
+}
+console.log('dirija com segurança!')
